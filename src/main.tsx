e9be0db546fada2ae70e334592bf77a55a7edd83
yijianwei <1689899299@qq.com>
2025-11-27 16:23:12 +0800

    🐛 fix: ，，

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles/theme.css'
 import './styles/global.css'
-import './styles/animations.css'
-
-// 生产环境使用 HashRouter，避免 GitHub Pages 对 SPA 路由 404
-const Router = import.meta.env.PROD ? HashRouter : BrowserRouter
-const basename = import.meta.env.BASE_URL
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router basename={basename}>
+    <BrowserRouter>
       <App />
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 )
